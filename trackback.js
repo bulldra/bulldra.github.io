61f48af2078f0363eb840762323bc62dd0a28909
bulldra <bulldra@gmail.com>
2014-10-09 22:29:37 +0900

    trackback.js

--- trackback.js
+++ trackback.js
@@ -16,7 +16,7 @@ function callbackBookmark(bookmark) {
   if(bookmark) {
     var eid = bookmark.eid;
     if(eid == '') {
-      return;
+      runTrackBack(null); 
     }
     var relUrl = 'http://b.hatena.ne.jp/fragments/entry.reldiary?'
                + '&key=fragments%2Fentry.reldiary.html%3Aeid%3D'
@@ -27,6 +27,8 @@ function callbackBookmark(bookmark) {
                + '&blog=' + getBlogUrl();
     console.log(relTrackbackRssUrl);
     runTrackBack(relTrackbackRssUrl); 
+  } else {
+    runTrackBack(null); 
   }
 }
 
@@ -34,15 +36,20 @@ function runTrackBack(relTrackbackRssUrl) {
   var header = '<h3>この記事への言及<a href="http://blog.hatena.ne.jp/my/edit?title=モノ申す&body=[' + document.location.href + ':embed]" target="_blank" style="float:right">＞＞言及する</a></h3>'
   var defaultHtml =  "<p>この記事への言及はありません。</p><br/>";
 
-  var feed = new google.feeds.Feed(relTrackbackRssUrl);
+  if(relTrackbackRssUrl != null) {
+    var feed = new google.feeds.Feed(relTrackbackRssUrl);
   
-  feed.setNumEntries(10);
-  feed.load(function(result) {
-    var r = new Array();
-    if (!result.error) {
-      r = result.feed.entries;
-    } 
-    createHtml(trackback_entry, header, r, defaultHtml, 10);
-  }); 
+    feed.setNumEntries(10);
+    feed.load(function(result) {
+      var r = new Array();
+      if (!result.error) {
+        r = result.feed.entries;
+      } 
+      createHtml(trackback_entry, header, r, defaultHtml, 10);
+    }); 
+  } else {
+      var r = new Array();
+      createHtml(trackback_entry, header, r, defaultHtml, 10);
+  }
 }
 
