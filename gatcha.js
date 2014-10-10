8057b2b68c05970f0b74e53def2921eb547c40e4
bulldra <bulldra@gmail.com>
2014-10-11 07:30:59 +0900

    trackback.js

--- gatcha.js
+++ gatcha.js
@@ -1,34 +1,37 @@
 google.load("feeds", "1");
 
+function writeGatcha(fetchMaxNum, startNum) {
+  document.write('<select id="inGatchaNum">');
+  for(var i = 1; i <= fetchMaxNum; i++) {
+    document.write('<option value="' + i + '"');
+    if(i == startNum) {
+      document.write(' selected');
+    }
+    document.write('>' + i + '</option>');
+  }
+  document.write('</select>');
+			  
+  document.write('<input type="button" id="btnNormalGatcha" value=" 通常ガチャ " onClick="runNormalGatcha()" /> <input type="button" id="btnRareGatcha" value=" レアガチャ " onClick="runRareGatcha()" />');
+
+  document.write('<span id="sideGatcha"></span>');
+  switchGatchButton(false);
+}
+
 function switchGatchButton(is) {
   btnRareGatcha.disabled = !is;
   btnNormalGatcha.disabled = !is;
 }
 
-function writeGatcha() {
-  document.write('<option"=');
 
-  document.write('<select id="inGatchaNum">');
-  for(var i = 1; i <= 5; i++) {
-    document.write('<option value="' + i + '">' + i + '</option>');
-  }
-  document.write('</select>');
-			  
-  document.write('<input type="button" id="btnNormalGatch" value=" 過去記事ガチャ " onClick="runNormalGatcha()" /> <input type="button" id="btnRaregatcha" value=" レアガチャ " onClick="runRareGatcha()" />
-
-  document.write('<span id="sideGatcha"></span>');
-}
-
-switchGatchButton(false);
-
-function runSideRareGatcha() {
+function runRareGatcha() {
   switchGatchButton(false);
   var fetchNum = inGatchaNum.value;
+  var blogUrl = getBlogUrl();
 
   var relRssUrl = 'http://b.hatena.ne.jp/entrylist?'
     + 'sort=count'
     + '&mode=rss'
-    + '&url=' + encodeURIComponent(blogURL);
+    + '&url=' + encodeURIComponent(blogUrl);
     
     var feed = new google.feeds.Feed(relRssUrl);
     feed.setNumEntries(30);
@@ -43,15 +46,16 @@ function runSideRareGatcha() {
   switchGatchButton(true);
 }
 
-function runSideGatcha() {
+function runNormalGatcha() {
   switchGatchButton(false);
   var fetchNum = inGatchaNum.value;
   var maxNum = 10;
+  var blogUrl = getBlogUrl();
 
   var feeds = new Array();
   for (var i = 1; i <= maxNum; i++) {
     var rssUrl = 'http://pipes.yahoo.com/pipes/pipe.run?url='
-            + encodeURIComponent(blogURL + '/sitemap.xml?page=' + i)
+            + encodeURIComponent(blogUrl + '/sitemap.xml?page=' + i)
             + '&_id=f48751a13b81b177ba69aa786f7354bf&_render=rss'
     feeds.push(new google.feeds.Feed(rssUrl));
   }
@@ -76,5 +80,5 @@ function runSideGatcha() {
   switchGatchButton(true);
 }
 
-google.setOnLoadCallback(runSideRareGatcha);
+google.setOnLoadCallback(runRareGatcha);
 
