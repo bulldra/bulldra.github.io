6f8ba446f513003e827d8ccc8e4dc362e34d9bdf
bulldra <bulldra@gmail.com>
2014-10-13 21:33:31 +0900

    シャッフル除去・html生成共有

--- commons.js
+++ commons.js
@@ -1,70 +1,25 @@
-function createHtml(container, headerHtml, resultEntries, defaultHtml,fetchNum) {
-  Array.prototype.shuffle = function() {
-    var i = this.length;
-    while(i){
-      var j = Math.floor(Math.random()*i);
-      var t = this[--i];
-      this[i] = this[j];
-      this[j] = t;
-    }
-    return this;
-  }
+function createHtml(container, headerHtml, resultEntries, 
+		defaultHtml,fetchNum) {
+  createCommonHtml(createEmbedFrame, container, headerHtml, 
+		  resultEntries, defaultHtml,fetchNum);
+}
 
+function createOwnHtml(container, headerHtml, resultEntries, 
+                defaultHtml,fetchNum) {
+  createCommonHtml(createOwnEmbedFrame, container, headerHtml, 
+		  resultEntries, defaultHtml,fetchNum);
+}  
+
+function createCommonHtml(func, container, headerHtml, resultEntries, defaultHtml,fetchNum) {
   var resultHtml = "";
   if (resultEntries.length == 0) {
     if(defaultHtml != null) {
       resultHtml = defaultHtml;
     } 
   } else {
-    resultEntries.shuffle();
+    resultEntries = shuffleArray(resultEntries);
     for(var x = 0; x < resultEntries.length && x < fetchNum; x++) { 
-      resultHtml += createEmbedFrame(resultEntries[x].link);
-    }
-  }
-
-  if(headerHtml != null) {
-    container.innerHTML = headerHtml;
-  } else {
-    container.innerHTML = '';
-  }
-  container.innerHTML += resultHtml;
-}
-
-function createEmbedFrame(link) {
-    if(link == null) {
-	    return "";
-    }
-    var embedURL = "http://hatenablog.com/embed?url=" 
-      + encodeURI(link);
-    var html = '<p><iframe src="'
-      + embedURL
-      + '" width="100%" height="160px" scrolling="no" '
-      + 'frameborder="0" style="margin:3px 0px;">'
-      + '</iframe></p>';
-    return html;
-}
-
-function createOwnHtml(container, headerHtml, resultEntries, defaultHtml,fetchNum) {
-  Array.prototype.shuffle = function() {
-    var i = this.length;
-    while(i){
-      var j = Math.floor(Math.random()*i);
-      var t = this[--i];
-      this[i] = this[j];
-      this[j] = t;
-    }
-    return this;
-  }
-
-  var resultHtml = "";
-  if (resultEntries.length == 0) {
-    if(defaultHtml != null) {
-      resultHtml = defaultHtml;
-    } 
-  } else {
-    resultEntries.shuffle();
-    for(var x = 0; x < resultEntries.length && x < fetchNum; x++) { 
-      resultHtml += createOwnEmbedFrame(resultEntries[x].link);
+      resultHtml += func(resultEntries[x].link);
     }
   }
   if(headerHtml != null) {
@@ -98,6 +53,31 @@ function getBlogUrl() {
   }
   var ret = href.replace(/(^http:\/\/[^\/]*).*/,"$1");
   return ret;
+}
+
+function createEmbedFrame(link) {
+    if(link == null) {
+	    return "";
+    }
+    var embedURL = "http://hatenablog.com/embed?url=" 
+      + encodeURI(link);
+    var html = '<p><iframe src="'
+      + embedURL
+      + '" width="100%" height="160px" scrolling="no" '
+      + 'frameborder="0" style="margin:3px 0px;">'
+      + '</iframe></p>';
+    return html;
+}
+
+function shuffleArray(array) {
+    var i = array.length;
+    while(i){
+      var j = Math.floor(Math.random()*i);
+      var t = array[--i];
+      array[i] = array[j];
+      array[j] = t;
+    }
+    return array;
 }
 
 function removeThisEntry(entries) {
