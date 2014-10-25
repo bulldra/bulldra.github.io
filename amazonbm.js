62c8e09e1c25fc538f1b493ff6b8f3aade534198
bulldra <bulldra@gmail.com>
2014-10-26 01:08:55 +0900

    amazon

--- amazonbm.js
+++ amazonbm.js
@@ -1,13 +1,12 @@
 ﻿function amazonLink(asosiateid, width) {
   var p = document.getElementById('prodImage');
   var img = p.src.replace(/\.[^\.]+\.jpg/,'.SX' + width + '.jpg');
-
-  var l = document.getElementsByTagName("link");
   var url = location.href; 
 
+  var l = document.getElementsByTagName("link");
   for (var i = 0; i < l.length; i++) {
     if (l[i].rel === "canonical") {
-      url = l[i];
+      url = l[i].href;
       break;
     }
   }
