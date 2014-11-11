d2d94b8e80f1bd67b938aa3684c0a31b5bd46b4c
bulldra <bulldra@gmail.com>
2014-11-11 18:53:09 +0900

    bookmarkembed

--- commons.js
+++ commons.js
@@ -132,6 +132,7 @@ function setBookmarkEmbedWidth(width) {
   if(embeds != null) {
     for (var i = 0; i < embeds.length; i++) {
       embeds[i].style.maxWidth = width + "px";
+      embeds[i].style.margin = "0px";
     }
   }
 }
