c4cba714d873a151d445f9753b22a2b94afd2845
bulldra <bulldra@gmail.com>
2014-11-14 06:33:08 +0900

    clander

---
+++ calender.js
@@ -0,0 +1,15 @@
+﻿function transformCalender() {
+  var ws = document.querySelectorAll('.archive-module-calendar-week');   
+  if(ws != null) {
+      var w = ws[ws.length - 1];
+      var c = w.childNodes;
+      for(var i = c.length - 1; i < 7; i++) {
+        var td = document.createElement('td');
+        td.className = 'calendar-day';
+        c.appendNode(td);
+      }
+  }
+}
+
+google.setOnLoadCallback(transformCalender);
+
