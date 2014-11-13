0cb1eb78799dcf55f4a7348bcd7e44869089d53c
bulldra <bulldra@gmail.com>
2014-11-14 06:49:33 +0900

    clander

--- calender.js
+++ calender.js
@@ -4,13 +4,13 @@
       var w = ws[ws.length - 1];
       var c = w.childNodes;
       console.log(c.length);
-
+/*
       for(var i = c.length - 1; i <= 7; i++) {
         var td = document.createElement('td');
         td.className = 'calendar-day';
         w.appendChild(td);
       }
-      console.log(w);
+      */
   }
 }
 
