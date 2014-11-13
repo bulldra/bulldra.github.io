3bf65bfaeead54f624a6b147e5ad7f82422ca176
bulldra <bulldra@gmail.com>
2014-11-14 07:05:04 +0900

    clander

--- calender.js
+++ calender.js
@@ -1,16 +1,25 @@
 ﻿function transformCalender() {
+
+  var d = document.querySelectorAll('.calendar-day span');
+  for (var i = 0; i < d.length; i++) {
+	  if(d[i].innerText.match(/[0-9]/) {
+		  d[i].innerText = '0' + d[i].innerText;
+	  }
+  }
+
+
+  /* 末尾セル追加 */
   var table = document.querySelector('.js-archive-module-calendar-container table');   
   if(table != null) {
       var tr = table.rows[table.rows.length - 1];
-      console.log(tr);
-      console.log(tr.cells.length);
-      
-      for(var i = tr.cells.length - 1; i < 7; i++) {
+      for(var i = tr.cells.length; i < 7; i++) {
         var td = document.createElement('td');
         td.className = 'calendar-day';
         tr.appendChild(td);
       }
   }
+
+
 }
 
 google.setOnLoadCallback(transformCalender);
