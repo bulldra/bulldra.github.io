e505dced2d081ba71a23ef25eb1502682d8fef6e
bulldra <bulldra@gmail.com>
2014-11-14 06:38:04 +0900

    clander

--- calender.js
+++ calender.js
@@ -3,7 +3,9 @@
   if(ws != null) {
       var w = ws[ws.length - 1];
       var c = w.childNodes;
-      for(var i = c.length - 1; i < 7; i++) {
+      console.log(w);
+
+      for(var i = c.length - 1; i <= 7; i++) {
         var td = document.createElement('td');
         td.className = 'calendar-day';
         w.appendChild(td);
