d6207c703b1f9d85894f78d03e18baf495dd8b74
bulldra <bulldra@gmail.com>
2014-11-14 09:15:17 +0900

    clendar

--- calendar.js
+++ calendar.js
@@ -28,7 +28,8 @@ function addTransformCalendar() {
 	transformCalendar();
 	var s = document.querySelector('.archive-module-calendar-selector')
 	if(s != null) {
-		s.addEventListener('change',  transformCalendar, false);
+	   s.addEventListener('change',  transformCalendar, false);
+	   console.log(s);
 	}
 	console.log(s);
 }
