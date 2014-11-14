077eb18ab5c0d443692cadfe7395e576fc11dfab
bulldra <bulldra@gmail.com>
2014-11-14 16:21:51 +0900

    clendar

--- calendar.js
+++ calendar.js
@@ -83,7 +83,7 @@ function addTransformCalendar() {
         var url = $(this).find('a').attr("href");
 	console.log(url);
         var imgurl = 'http://capture.heartrails.com/300x250/shadow?' + url;
-        $('#image-calender').src = imgurl;
+        document.querySelector('#image-calender').src = imgurl;
       }, function(e) { } );
   });
 }
