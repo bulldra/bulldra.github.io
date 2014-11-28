c752f6135291d8cfba4312e939b734bdf4b5e1c6
bulldra <bulldra@gmail.com>
2014-11-28 21:10:08 +0900

    sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,23 @@
+google.load("jquery", "1.7.1");
+google.setOnLoadCallback(sitemap);
+
+function sitemap(){
+   var siteMapUrl = getBlogUrl() + 'sitemap.xml';
+
+    $.ajax({ url: siteMapUrl, type:'GET', dataType:'xml', timeout:1000,
+        error:function() { console.log(siteMapUrl + "の取得に失敗しました"); }, 
+	success:function(xml){
+          var $locs = $(xml).find("loc");
+	  var loc = $locs.get(Math.floor($locs.size() * Math.random()));
+	  console.log(loc);
+	  var url = $(loc).text();
+          $.ajax({ url:url, type:'GET', dataType:'xml', timeout:1000,
+            error:function() { console.log(url + "の取得に失敗しました"); },
+            success:function(xml){
+              console.log(xml)
+	    }
+	  });
+       }
+    });
+}
+
