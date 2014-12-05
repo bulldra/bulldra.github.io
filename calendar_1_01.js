f445b73eac922f0e725f34d477cba05fe9c0efbc
bulldra <bulldra@gmail.com>
2014-12-06 07:55:35 +0900

    moreread.js

--- calendar_1_01.js
+++ calendar_1_01.js
@@ -15,21 +15,6 @@ function initTransformCalendar() {
   window.addEventListener("DOMContentLoaded", function(){
     setTimeout("transformNotify();", 200);
   }, false);
-  var $selector = $('select.js-archive-module-calendar-selector');
-  transformCalendar();
-  $selector.change(function () { transformNotify(); });
-}
-
-function transformCalendar() {
-  var table = document.querySelector('.js-archive-module-calendar-container table');   
-  if(table != null) {
-      var tr = table.rows[table.rows.length - 1];
-      for(var i = tr.cells.length; i < 7; i++) {
-        var td = document.createElement('td');
-        td.className = 'calendar-day';
-        tr.appendChild(td);
-      }
-  }
   
   var $in = $('<input type="button" value="一覧表示" />');
   $in.on('click',function() {
@@ -42,12 +27,30 @@ function transformCalendar() {
   }, false);
   $c.append($in);
   
+  var $selector = $('select.js-archive-module-calendar-selector');
+  $selector.change(function () { transformNotify(); });
+  transformCalendar();
+}
+
+function transformCalendar() {
+  var table = document.querySelector('.js-archive-module-calendar-container table');   
+
+  if(table != null) {
+      var tr = table.rows[table.rows.length - 1];
+      for(var i = tr.cells.length; i < 7; i++) {
+        var td = document.createElement('td');
+        td.className = 'calendar-day';
+        tr.appendChild(td);
+      }
+  }
+  
   var d = document.querySelectorAll('.calendar-day span');
   for (var i = 0; i < d.length; i++) {
     if(d[i].innerText.match(/^[0-9]$/)) {
       d[i].innerText = '0' + d[i].innerText;
     }
   }
+
   var ed = document.querySelectorAll('.calendar-day a');
   for (var i = 0; i < ed.length; i++) {
     ed[i].parentNode.className += ' calendar-day-entry'
