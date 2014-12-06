112b4f32b56e8b973b009d0a473f7a8904ab94fe
bulldra <bulldra@gmail.com>
2014-12-06 09:18:55 +0900

    スタイル名修正

--- parallax.js
+++ parallax.js
@@ -2,12 +2,12 @@ google.load("jquery", "1.7.1");
 google.setOnLoadCallback(setParallax);
 
 function setParallax() {
-  var $div = $('<div class="background"></div>');
+  var $div = $('<div class="background-parallax"></div>');
   $div.appendTo('body');
 
   $(window).scroll(function(e){ 
     var scrolled = $(window).scrollTop();
-    var $bg = $('.background');
+    var $bg = $('.background-parallax');
     $bg.css('top', -(scrolled * 0.1) + 'px');
   });
 }
