0ae30dbcf3589061aa6c3754194330bc9c1ff653
bulldra <bulldra@gmail.com>
2016-10-08 19:01:14 +0900

    Create wikipedia_annotaion.js

---
+++ wikipedia_annotaion.js
@@ -0,0 +1,57 @@
+function execWikipediaAnnotation() {
+  var word_set = new Set();
+  var segmenter = new TinySegmenter(); 
+  var objs = document.getElementsByClassName('wikipedia_annotaion');
+   if(objs == null || objs.length == 0) {
+     return;
+   }
+
+  for(var a in objs) { 
+    var segs = segmenter.segment(objs[a].innerText);
+     for(var b in segs) {
+       if(segs[b].length >= 3) {
+         word_set.add(segs[b]);
+       }
+     }
+  }
+
+  console.log(word_set);
+  for (let word of word_set) {
+    callWikipediaApi(word);
+  }
+}
+
+function callWikipediaApi(q) {
+  var api = '//wikipedia.simpleapi.net/api?output=jsonp&callback=callbackWikipediaApi&q=' 
+      + encodeURI(q);
+  console.log(api);
+
+  var s = document.createElement("script");
+  s.src = api;
+  document.getElementsByTagName("script")[0].parentNode.appendChild(s); 
+}
+
+function callbackWikipediaApi(args) {
+  if(args == null || args.length == 0 || args[0] == null) {
+    console.log('args is null');
+    return;
+  }
+  var r = args[0];
+
+  var objs = document.getElementsByClassName('wikipedia_annotaion');
+  if(objs == null || objs.length == 0) {
+    console.log('objs is null');
+    return;
+  }
+  console.log(objs)
+  for(var a in objs) {
+    console.log(objs[a].innerHTML);
+    var text = objs[a].innerHTML;
+    if(text == null || text.length <= 1) {
+      console.log('text is null');
+      continue;
+    }    
+    objs[a].innerHTML = text.replace(r.title, r.title + '<sup title="' + r.body + '">※</sup>', 'g');
+    console.log(objs[a].innerHTML);
+  }
+}
